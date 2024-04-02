93d9208d818b5aed74221d91315b98dba442926b
Olavi Raigla <oraigla@tktk.ee>
2024-04-02 15:46:09 +0300

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Mr. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+//= Char.Parse tähendab seda, et muudab info kohe char kujule
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.  {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+//kui if plok n läbi tehtud ja kood on ära teinud, siis kood enam vahemälus ei eksisteeri
+//"'" ülakomad on reserveeritud char'dele ja jutumärgid on stringidele
+//else if hoiab ära selle, et peale positiivse vastuse saamist ta teeb ka järgmised kontrollid
